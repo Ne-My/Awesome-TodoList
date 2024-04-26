773569848634725ea4efc9f379611fd96750ccc1
Ne My <nemyne717@gmail.com>
2024-04-26 20:44:19 +0430

    init tailwindcss

---
+++ tailwind.config.ts
@@ -0,0 +1,13 @@
+import type { Config } from 'tailwindcss'
+
+export default {
+  content: ['./src/**/*.{vue,ts,js}'],
+  theme: {
+    fontFamily: {
+      poppins: 'poppins'
+    },
+    extend: {},
+  },
+  plugins: [],
+} satisfies Config
+
