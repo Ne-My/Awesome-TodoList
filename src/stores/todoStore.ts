27c53899e450fd7a824c409b79fb2c9c52946d81
Ne My <nemyne717@gmail.com>
2025-02-20 19:46:26 +0330

    Feat ( Store Todo ): add todoAdd item, todoChangeState, todoDelete, all todos count, completedTodosCount and leftTodosCount

--- src/stores/todoStore.ts
+++ src/stores/todoStore.ts
@@ -1,51 +1,49 @@
 import type { Todo } from '@/types'
 import { defineStore } from 'pinia'
 import { v4 } from 'uuid'
-import { reactive } from 'vue'
+import { computed, reactive } from 'vue'
 
 export const useTodoStore = defineStore('todo', () => {
-	const todos = reactive<Todo[]>([
-		{
-			id: 1,
-			name: 'Go to the gym!',
-			time: '16:30 - 18:00',
-			icon: '/emojies/image 1.png',
-			checked: false,
-		},
-	])
+  const todos = reactive<Todo[]>([
+    {
+      id: 'first',
+      name: 'Go to the gym!',
+      checked: false,
+    },
+  ])
 
-	const addTodoItem = ({
-		name,
-		time = '',
-		icon = '',
-		checked = false,
-	}: {
-		name: string
-		time?: string
-		icon?: string
-		checked: boolean
-	}): void => {
-		todos.push({
-			id: v4(),
-			name,
-			icon,
-			time,
-			checked,
-		})
-	}
+  const allTodosCount = computed(() => todos.length)
+  const completedTodosCount = computed(() => todos.filter((obj) => obj.checked === true).length)
+  const leftTodosCount = computed(() => allTodosCount.value - completedTodosCount.value)
 
-	const checkTodoItem = (
-		id: number | string,
-		state: boolean = true
-	): boolean => {
-		const todo: Todo | undefined = todos.find((obj: Todo) => obj.id === id)
-		if (!todo) return false
-		todo.checked = state
-		return true
-	}
-	return {
-		todos,
-		addTodoItem,
-		checkTodoItem,
-	}
+  const todoAddItem = (name: string, checked: boolean = false): void => {
+    todos.push({
+      id: v4(),
+      name,
+      checked,
+    })
+  }
+
+  const todoChangeState = (id: string, checked: boolean) => {
+    todos.find((obj) => obj.id === id)!.checked = checked
+  }
+
+  const TodoDelete = (id: string): boolean => {
+    const todoIndex = todos.findIndex((obj) => obj.id === id)
+    if (!todoIndex) return false
+    todos.splice(todoIndex, 1)
+    return true
+  }
+
+  return {
+    todos,
+
+    allTodosCount,
+    completedTodosCount,
+    leftTodosCount,
+
+    todoAddItem,
+    todoChangeState,
+    TodoDelete,
+  }
 })
