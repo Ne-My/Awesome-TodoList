18c66d63f691dbb77a35c9c70a6ce7ce3a17c3a2
Ne My <nemyne717@gmail.com>
2025-02-20 19:48:16 +0330

    Refactor: changes folder name from views to pages

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,16 +1,13 @@
-import HomeView from '@/views/HomeView.vue'
-import {
-	createRouter,
-	createWebHistory,
-} from 'vue-router'
+import HomeView from '@/pages/HomeView.vue'
+import { createRouter, createWebHistory } from 'vue-router'
 
 export const router = createRouter({
-	history: createWebHistory(),
-	routes: [
-		{
-			path: '',
-			name: 'home',
-			component: HomeView,
-		},
-	],
+  history: createWebHistory(),
+  routes: [
+    {
+      path: '',
+      name: 'home',
+      component: HomeView,
+    },
+  ],
 })
