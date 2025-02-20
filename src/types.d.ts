a82eaea037ab6942714013bed9802dd2908773ec
Ne My <nemyne717@gmail.com>
2025-02-20 19:43:46 +0330

    Chore ( Types ): change todo item id type to only string

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,9 +1,9 @@
 type IconList = ''
 
 export type Todo = {
-	id: number | string,
-	name: string,
-	icon?: string,
-	time?: string,
-	checked: boolean
+  id: string
+  name: string
+  icon?: string
+  time?: string
+  checked: boolean
 }
