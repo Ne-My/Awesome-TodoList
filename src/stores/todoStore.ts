b891e12847c691344dd589f66887d40222628c0d
Ne My <nemyne717@gmail.com>
2025-02-20 20:32:17 +0330

    Fix ( Store Todo ): change todos from reactive to ref to be able to use persistedstate pinia plugin ability

--- src/stores/todoStore.ts
+++ src/stores/todoStore.ts
@@ -1,25 +1,19 @@
 import type { Todo } from '@/types'
 import { defineStore } from 'pinia'
 import { v4 as uuidv4 } from 'uuid'
-import { computed, reactive } from 'vue'
+import { computed, ref } from 'vue'
 
 export const useTodoStore = defineStore(
   'todo',
   () => {
-    const todos = reactive<Todo[]>([
-      {
-        id: 'first',
-        name: 'Go to the gym!',
-        checked: false,
-      },
-    ])
+    const todos = ref<Todo[]>([])
 
-    const allTodosCount = computed(() => todos.length)
-    const completedTodosCount = computed(() => todos.filter((obj) => obj.checked === true).length)
+    const allTodosCount = computed(() => todos.value.length)
+    const completedTodosCount = computed(() => todos.value.filter((obj) => obj.checked === true).length)
     const leftTodosCount = computed(() => allTodosCount.value - completedTodosCount.value)
 
     const todoAddItem = (name: string, checked: boolean = false): void => {
-      todos.push({
+      todos.value.push({
         id: uuidv4(),
         name,
         checked,
@@ -27,13 +21,13 @@ export const useTodoStore = defineStore(
     }
 
     const todoChangeState = (id: string, checked: boolean) => {
-      todos.find((obj) => obj.id === id)!.checked = checked
+      todos.value.find((obj) => obj.id === id)!.checked = checked
     }
 
     const TodoDelete = (id: string): boolean => {
-      const todoIndex = todos.findIndex((obj) => obj.id === id)
+      const todoIndex = todos.value.findIndex((obj) => obj.id === id)
       if (!todoIndex) return false
-      todos.splice(todoIndex, 1)
+      todos.value.splice(todoIndex, 1)
       return true
     }
 
