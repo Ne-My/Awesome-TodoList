b4583227e02b4f3ccbd533f4c43ae4cc713e8fb2
Ne My <nemyne717@gmail.com>
2025-02-20 19:56:33 +0330

    Refactor: move _components folder component to components folder and rename them

--- src/pages/HomeView.vue
+++ src/pages/HomeView.vue
@@ -1,17 +1,17 @@
 <script setup lang="ts">
-import HomeViewHeader from './_partials/HomeViewHeader.vue'
+import Header from '@/components/Header.vue'
 // @ts-ignore
-import HomeViewInput from './_partials/HomeViewInput.vue'
+import AddTodoField from '@/components/AddTodoField.vue'
 // @ts-ignore
-import TodoList from '../components/TodoList.vue'
+import TodoList from '@/components/TodoList.vue'
 </script>
 
 <template>
   <div class="flex flex-col gap-9 bg-white rounded-xl shadow-md min-w-[500px] p-8 pb-12 relative">
-    <HomeViewHeader />
+    <Header />
 
     <TodoList />
 
-    <HomeViewInput />
+    <AddTodoField />
   </div>
 </template>
