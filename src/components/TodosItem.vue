9f86819412cd2df53e9a3d7c1d85d21b11682c3d
Ne My <nemyne717@gmail.com>
2025-02-21 21:52:46 +0330

    Feat ( TodosItem ): move item from uncompleted todo list to completed list and versa vice

--- src/components/TodosItem.vue
+++ src/components/TodosItem.vue
@@ -3,23 +3,39 @@ import { useTodoStore } from '@/stores/todoStore'
 import IconTrash from '@/components/icons/IconTrash.vue'
 import IconCheck from '@/components/icons/IconCheck.vue'
 import type { Todo } from '@/types'
-import { toRefs } from 'vue'
+import { toRefs, type HTMLAttributes } from 'vue'
+import { storeToRefs } from 'pinia'
 
-const props = defineProps<{ todo: Todo }>()
+const props = defineProps<{ todo: Todo; class: HTMLAttributes['class'] }>()
 const { id, name, checked } = toRefs(props.todo)
 
 const todoStore = useTodoStore()
+const { deleteTodo } = todoStore
+const { completedTodos, unCompletedTodos } = storeToRefs(todoStore)
+
+function checkItem() {
+  const isTodoChecked = !checked.value
+  checked.value = isTodoChecked
+  if (isTodoChecked) {
+    unCompletedTodos.value = unCompletedTodos.value.filter((item) => item.id !== id.value)
+    completedTodos.value.unshift(props.todo)
+  } else {
+    completedTodos.value = completedTodos.value.filter((item) => item.id !== id.value)
+    unCompletedTodos.value.unshift(props.todo)
+  }
+}
 </script>
 
 <template>
   <div
     class="group bg-white flex items-center justify-between gap-4 rounded-xl shadow-lg px-4 py-3 cursor-pointer border border-slate-300 overflow-hidden"
-    @click="checked = !checked"
+    :class="props.class"
   >
     <div class="flex items-center gap-2 overflow-hidden w-full">
       <div
         class="shrink-0 size-6 bg-slate-50 border border-slate-300 rounded-md flex items-center justify-center"
         :class="{ 'bg-emerald-200 border-0': checked }"
+        @click="checkItem"
       >
         <IconCheck v-if="checked" class="size-4 [&>path]:stroke-emerald-500" />
       </div>
@@ -33,7 +49,7 @@ const todoStore = useTodoStore()
     </div>
 
     <div>
-      <IconTrash class="cursor-pointer size-5 [&>path]:hover:stroke-rose-600" @click="todoStore.TodoDelete(id)" />
+      <IconTrash class="cursor-pointer size-5 [&>path]:hover:stroke-rose-600" @click="deleteTodo(id)" />
     </div>
   </div>
 </template>
