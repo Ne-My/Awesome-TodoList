3ecef86267c5e5e3c389993451ff0f7d09172332
Ne My <nemyne717@gmail.com>
2025-02-21 21:48:10 +0330

    Chore ( Tailwind Config ): remove unused font

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,11 +3,7 @@ import type { Config } from 'tailwindcss'
 export default {
   content: ['./src/**/*.{vue,ts,js}'],
   theme: {
-    fontFamily: {
-      poppins: 'poppins'
-    },
     extend: {},
   },
   plugins: [],
 } satisfies Config
-
