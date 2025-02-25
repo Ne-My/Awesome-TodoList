f8d673457e4bbad5783f0b2c05adcdb42f35a200
Ne My <nemyne717@gmail.com>
2025-02-25 09:12:58 +0330

    Feat ( Store Todo ): move checked todos to end of the list

--- src/stores/todoStore.ts
+++ src/stores/todoStore.ts
@@ -1,12 +1,27 @@
 import type { Todo } from '@/types'
 import { defineStore } from 'pinia'
 import { v4 as uuidv4 } from 'uuid'
-import { ref } from 'vue'
+import { nextTick, ref, watch } from 'vue'
 
 export const useTodoStore = defineStore(
   'todo',
   () => {
     const todos = ref<Todo[]>([])
+
+    watch(
+      todos,
+      async (val) => {
+        await nextTick()
+        todos.value = val.sort((a, b) => {
+          if (a.checked && !b.checked) return 1
+          else if (b.checked && !a.checked) return -1
+          return 0
+        })
+      },
+      {
+        deep: true,
+      },
+    )
 
     const addTodo = (name: string, icon: number = 0): void => {
       todos.value.unshift({
