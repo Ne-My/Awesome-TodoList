0071d0f15da4417823ad0c601c7fb7e96757c2f8
Ne My <nemyne717@gmail.com>
2025-02-26 20:49:06 +0330

    Refactor: remove vue router

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,20 @@
 <script setup lang="ts">
+import Header from '@/components/Header.vue'
+import { TodoInput } from '@/components/TodoInput'
+import Todos from '@/components/Todos.vue'
 
+import { EditModal } from '@/components/EditModal'
 </script>
 
 <template>
-  <RouterView />
+  <EditModal />
+  <div class="w-screen h-screen overflow-hidden sm:grid place-items-center bg-indigo-500 sm:px-4">
+    <div
+      class="flex flex-col gap-4 bg-white sm:rounded-xl shadow-xs w-screen h-screen sm:h-auto sm:max-w-[500px] px-8 pt-4 pb-12 relative"
+    >
+      <Header />
+      <Todos />
+      <TodoInput />
+    </div>
+  </div>
 </template>
-
-<style scoped>
-
-</style>
