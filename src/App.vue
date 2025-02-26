8eff8a65b480f14a0aed27b21dcb5d9af8de0490
Ne My <nemyne717@gmail.com>
2025-02-27 01:26:19 +0330

    Fix: use dynamic viewport (dv) instead of vh and vw

--- src/App.vue
+++ src/App.vue
@@ -8,9 +8,9 @@ import { EditModal } from '@/components/EditModal'
 
 <template>
   <EditModal />
-  <div class="w-screen h-screen overflow-hidden sm:grid place-items-center bg-indigo-500 sm:px-4">
+  <div class="w-[100dvw] h-[100dvh] overflow-hidden sm:grid place-items-center bg-indigo-500 sm:px-4">
     <div
-      class="flex flex-col gap-4 bg-white sm:rounded-xl shadow-xs w-screen h-screen sm:h-auto sm:max-w-[500px] px-8 pt-4 pb-12 relative"
+      class="flex flex-col gap-4 bg-white sm:rounded-xl shadow-xs w-screen h-full sm:h-auto sm:max-w-[500px] px-8 pt-4 pb-12 relative"
     >
       <Header />
       <Todos />
